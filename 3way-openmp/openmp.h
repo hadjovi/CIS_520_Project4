049ddfa2ef8fc212f54ee993ca6a62ab04bc18cd
joshuapeterson4 <69777940+joshuapeterson4@users.noreply.github.com>
2023-04-30 17:15:23 -0500

    Update openmp.h

--- 3way-openmp/openmp.h
+++ 3way-openmp/openmp.h
@@ -7,7 +7,6 @@ void outPrint();
 //Function to open and run through the file char by char and separate out all the lines
 void frontRunner();
 
-
 //Function to look at specific lines and save the highest char value of that line to an array
 void parallelPart(int myID);
 
